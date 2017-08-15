f1367acbefcec9c5917ea0926aa570f2c8ec05aa
leilina <leilina@xiaoneng.cn>
2017-08-15 18:45:27 +0800

    公司详情

--- src/script/controller/companyCtrl.js
+++ src/script/controller/companyCtrl.js
@@ -1,4 +1,8 @@
 "use strict";
-angular.module("app").controller("companyCtrl",["$scope",function($scope){
+angular.module("app").controller("companyCtrl",["$state","$http","$scope",function($state,$http,$scope){//依赖注入后，对应的变量位置不能改变
+    $http.get("/data/company.json?id="+$state.params.id).then(function(res){
+        $scope.company = res.data;
+        $scope.isActive = true;
+    })
 
 }]);
--- src/script/directive/positionClass.js
+++ src/script/directive/positionClass.js
@@ -3,6 +3,19 @@ angular.module("app").directive("appPositionClass",[function(){
     return {
         restrict:"A",
         replce:true,
-        templateUrl:"view/template/positionClass.html"
+        templateUrl:"view/template/positionClass.html",
+        scope:{
+            "comp":"=",
+        },
+        link:function($scope){
+            $scope.isActive = 0;
+            console.log($scope.comp);
+            // $scope.positionLists = $scope.company.positionClass[0].positionList;
+            $scope.positionClassCurrent = function(index){
+                console.log( $scope.comp.positionClass[index]);
+                $scope.positionLists = $scope.comp.positionClass[index].positionList;
+                $scope.isActive = index;
+            }
+        }
     }
 }]);
