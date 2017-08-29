e6e7e2a70125a88ee5327d6f4826bf9ca24fd707
leilina <leilina@xiaoneng.cn>
2017-08-29 18:45:04 +0800

    登录

--- src/script/config/router.js
+++ src/script/config/router.js
@@ -17,6 +17,26 @@ angular.module("app").config(["$stateProvider","$urlRouterProvider",function($st
         url:"/search",
         templateUrl:"view/search.html",
         controller:"searchCtrl"
+    }).state("login",{
+        url:"/login",
+        templateUrl:"view/logi.html",
+        controller:"loginCtrl"
+    }).state("register",{
+        url:"/register",
+        templateUrl:"view/register.html",
+        controller:"registerCtrl"
+    }).state("me",{
+        url:"/me",
+        templateUrl:"view/me.html",
+        controller:"meCtrl"
+    }).state("post",{
+        url:"/post",
+        templateUrl:"view/post.html",
+        controller:"postCtrl"
+    }).state("favorite",{
+        url:"/favorite",
+        templateUrl:"view/favorite.html",
+        controller:"favoriteCtrl"
     });
     // 重定向url，如果访问的URL都不存在
     $urlRouterProvider.otherwise("main");
---
+++ src/script/controller/meCtrl.js
@@ -0,0 +1,4 @@
+"use strict";
+angular.module("app").controller("meCtrl",["$scope",function($scope){
+
+}]);
